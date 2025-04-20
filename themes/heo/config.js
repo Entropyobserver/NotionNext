13c910eb68dc062256680ae9a14ce96031ce3e37
J <sicper@163.com>
2025-04-20 14:45:47 +0200

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -18,19 +18,19 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
+  HEO_HERO_TITLE_1: 'Write smarter. Build faster. Innovate relentlessly.',
+  HEO_HERO_TITLE_2: 'Code like a pro. Think like a visionary.',
   HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_4: 'Turning bugs into breakthroughs.',
+  HEO_HERO_TITLE_5: 'Where innovation meets execution.',
+  HEO_HERO_TITLE_LINK: 'https://techenglish.top',
   // 英雄区遮罩文字
-  HEO_HERO_COVER_TITLE: '随便逛逛',
+  HEO_HERO_COVER_TITLE: 'Browse Freely',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_1: { title: 'Must-Read Selection', url: '/tag/Must-Read Selection' },
+  HEO_HERO_CATEGORY_2: { title: 'Popular Articles', url: '/tag/Popular Articles' },
+  HEO_HERO_CATEGORY_3: { title: 'Practical Tutorials', url: '/tag/Practical Tutorials' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
